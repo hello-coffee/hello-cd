47872a57934f989e66ecdc74761346a6e9f6a476
YuyaTanabe <gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.com>
2018-12-13 08:47:37 +0000

    住所、注文テーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_113544) do
+ActiveRecord::Schema.define(version: 2018_12_13_084306) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_113544) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -90,6 +91,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_113544) do
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "products", force: :cascade do |t|
