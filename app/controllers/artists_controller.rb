59c0fb6b1537e9d61f3afada538d47ba8a74337a
watarusan <w.w.shmw1@gmail.com>
2018-12-15 04:36:58 +0000

    [add]ページャ

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,8 @@
 class ArtistsController < ApplicationController
+
   def show
   	@artist = Artist.find(params[:id])
-  	@products = @artist.products
+  	@products = @artist.products.page(params[:page]).per(9).reverse_order
 
   	# 検索オブジェクト
     @search = Artist.ransack(params[:q])
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
   	@category = Category.find(params[:id])
-  	@products = @category.products
+  	@products = @category.products.page(params[:page]).per(9).reverse_order
   end
 end
