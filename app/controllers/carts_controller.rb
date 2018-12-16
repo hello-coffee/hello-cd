46a8a78fad5dfef34a28fa66c20e3c77216022ae
watarusan <w.w.shmw1@gmail.com>
2018-12-16 08:17:07 +0000

    部分テンプレ、カート、決済のビューとコントローラーを変更

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,22 @@
 class CartsController < ApplicationController
   def show
-  	@cart = Carts.find_by(user_id: current_user.id)
-  	if
-  		@    .save
+      @product = Products.find(params[:id])
+
+      @current_stock_array = []
+      @products.current_stock.times do |stock|
+    if stock < 10
+        @current_stock_array << [stock + 1, stock + 1]
+    else
+    break
+    end
+
+  	 @user = User.find(params[:id])
+  	 @cart = Carts.find_by(user_id: current_user.id)
+    if
+  		@cart.save
   		redirect_to new_oder_path
-  	else
+    else
   		render :show
-  	end
+    end
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,3 +19,4 @@ class OrdersController < ApplicationController
   def index
   end
 end
+
