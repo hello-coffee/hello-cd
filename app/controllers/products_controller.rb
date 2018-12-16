3c93b21272f701b3881b01ed69841ca9fa6938df
YuyaTanabe <gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.com>
2018-12-16 10:25:51 +0000

    produtoに関するコントローラー、モデル、ビューを変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,34 @@
 class ProductsController < ApplicationController
-  def index
-  end
+    def index
+    end
 
-  def show
-  end
+    def show
+    end
 
-  def update
-  end
+    def update
+    end
 
-  def destroy
-  end
+    def destroy
+    end
 
-  def edit
-  end
+    def edit
+    end
 
-  def new
-  end
+    def new
+        @category = Category.new
+        @artist = Artist.new
+        @product = Product.new
+    end
 
-  def create
-  end
+    def create
+        @product = Product.new(post_image_params) #paramsでは、フォームで入力されたデータが投稿データとして許可されているパラメータかチェックしている
+        @product.save
+        redirect_to product_path(@product.id)
+    end
+
+    private
+        def product_params
+            params.require(:product).permit(:artist_id, :title, :image, :price, :label, :category_id, :stock, :deleted_at)
+        end
+
 end
