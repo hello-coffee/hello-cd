4712a61d18f23e031aea416375bb7b8dcb04cdb2
watarusan <w.w.shmw1@gmail.com>
2018-12-18 09:37:25 +0000

    カート、アーティスト、カテゴリーコントローラ変更。検索結果一覧画面作成、不要なマイグレーションファイルを削除

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,10 @@
 class CategoriesController < ApplicationController
+
   def show
-  	@Category = Category.find(params[:id])
+  	@category = Category.find(params[:id])
   	@products = @category.products.page(params[:page]).per(9).reverse_order
 
   	@categories = Category.all
   end
 end
+
