abc9652d122ae43bac5f822ff6b24a0d71509641
Kei Yamaguchi <pop.l.key2@gmail.com>
2018-12-22 17:27:56 +0900

    sign_up時の郵便番号、メールアドレス、パスワードの入力規制、購入履歴の時間表記の変更、パスワード変更機能変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
+class PostCodeValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors.add(attribute, '郵便番号は、数字3桁＋ハイフン（-）＋数字4桁の形式で入力してください。') unless value =~ /\A[0-9]{3}-[0-9]{4}\z/
+  end
+end
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
-  acts_as_paranoid
+
+  	validates :password,
+  	format: { with: /\A[a-zA-Z]+\z/,
+    message: "パスワードは、英文字のみが使用できます。" }
+
+    validates :post_code,
+    post_code: true
+    validates :email,
+    presence: true
+  	acts_as_paranoid
 
 	has_many :reviews, dependent: :destroy
 	has_many :products, :through => :reviews
