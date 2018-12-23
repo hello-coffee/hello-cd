98512cca868a3682567f090160a08b37b7c25082
work <musashimiymoto@gmail.com>
2018-12-23 07:17:18 +0000

    管理者TOP編集

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,9 @@
 class AdminsController < ApplicationController
 
 	def show
+		@all_user = User.all
+		@all_product = Product.all
+		@news = News.new
 	end
 
 end
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,5 @@
 class NewsController < ApplicationController
+  
   def create
   end
 
