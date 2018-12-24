2b0695c28c032703e91e918ffbb025af6f978702
work <musashimiymoto@gmail.com>
2018-12-24 02:16:16 +0000

    TOP=>guidoeのroutesやNEWS投稿機能を編集しました

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,21 @@
 class NewsController < ApplicationController
   
   def create
+    news = News.new(news_params)
+    news.save
+    redirect_to products_path
   end
 
   def destroy
+    news = News.find(params[:id])
+    news.destroy
+    redirect_to products_path
   end
+  
+  
+  private
+  
+  def news_params
+    params.require(:news).permit(:body)
+  end 
 end
